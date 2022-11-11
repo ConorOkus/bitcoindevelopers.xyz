8ff879b3ed40c21e74379aaed6a6955fe1d3432b
Stephen DeLorme <stephen@d.elor.me>
2022-11-11 16:44:37 +0100

    Added dark mode toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,47 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  const [prefersDarkMode, setPrefersDarkMode] = React.useState(false)
+
+  const applyColorMode = ()=> {
+    if(prefersDarkMode && localStorage.darkMode === '0') {
+      setDarkMode(false)
+      document.documentElement.classList.remove('dark')
+    }
+    else if(prefersDarkMode || localStorage.darkMode === '1') {
+      setDarkMode(true)
+      document.documentElement.classList.add('dark')
+    }
+  }
+
+  React.useEffect(()=>{
+    if(window.matchMedia('(prefers-color-scheme: dark').matches) setPrefersDarkMode(true)
+    applyColorMode()
+  }, [prefersDarkMode])
+
+  const toggleDarkMode = () => {
+    if(localStorage.darkMode === '1') {
+      setDarkMode(false)
+      localStorage.darkMode = '0'
+      document.documentElement.classList.remove('dark')
+    }
+    else if(localStorage.darkMode === '0') {
+      setDarkMode(true)
+      localStorage.darkMode = '1'
+      document.documentElement.classList.add('dark')
+    }
+    else if(prefersDarkMode) {  
+      setDarkMode(false)
+      localStorage.darkMode = '0'
+      document.documentElement.classList.remove('dark')
+    }
+  }
+
   const navLinks = [
     {
       label: 'Twitch',
@@ -15,16 +54,19 @@ export default function Home() {
   ]
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Bitcoin Developers</title>
         <meta name="description" content="Bitcoin Developers is a Twitch stream focused on helping you learn bitcoin and lightning development from industry professionals." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main className="w-full">
         <header>
           Bitcoin Developers
+
+          <button onClick={toggleDarkMode}>Dark Mode</button>
+
           <ul>
             {navLinks.map((navLink, key)=>(
               <li>
